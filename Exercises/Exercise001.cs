4b329d7b14a2d24b86e1b470a9d1a06b6397c852
Nolan Wang <vitanuan@gmail.com>
2022-05-04 22:17:16 +0100

    finished E001

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Exercises.Models;
-
 namespace Exercises
 {
     public class Exercise001
@@ -9,31 +8,47 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            return char.ToUpper(word[0]) + word.Substring(1);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            return firstName[0] + "." + lastName[0];
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            return Math.Round(originalPrice * (100 + vatRate) / 100, 2);
         }
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            char[] arr = sentence.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
 
         public int CountLinuxUsers(List<User> users)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int count = 0;
+            foreach (var user in users)
+            {
+                if (user.Type == "Linux")
+                {
+                    count += 1;
+                }
+            };
+            return count;
+
         }
     }
 }
