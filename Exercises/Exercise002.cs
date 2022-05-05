80917ac075b8b18d0188a88224be90a3c3b17629
Nolan Wang <vitanuan@gmail.com>
2022-05-05 10:47:59 +0100

    finished E 002

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using Exercises.Models;
+
 namespace Exercises
 {
     public class Exercise002
     {
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester(Person person)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            if (person.City == "Manchester")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int limit = ageLimit;
+            if (person.Age < limit)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
+
---
+++ Exercises/Models/Person.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Exercises.Models
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, string city, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Age = age;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+        public int Age { get; set; }
+    }
+}
