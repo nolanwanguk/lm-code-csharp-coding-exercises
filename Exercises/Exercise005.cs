9b297296ff3f778280e236a3de57515a764b1d91
Nolan Wang <vitanuan@gmail.com>
2022-05-05 18:40:24 +0100

    non-elegant solution for E 005

--- Exercises/Exercise005.cs
+++ Exercises/Exercise005.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 namespace Exercises
 {
     public class Exercise005
@@ -6,7 +7,40 @@ namespace Exercises
         public bool IsPangram(string input)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            else if (input.ToLower() == "abcdefghijklmnopqrstuvwxyz")
+            {
+                return true;
+            }
+            else
+            {
+                if (input.Contains("_"))
+                {
+                    return true;
+                }
+
+                if (input.Any(char.IsNumber))
+                {
+                    return true;
+                }
+
+                string trim_input = input.Replace(" ", "");
+                if (!trim_input.ToLower().Contains("x") || !trim_input.ToLower().Contains("h"))
+                {
+                    return false;
+                }
+                if (trim_input.All(char.IsLower)||(trim_input.Any(char.IsLower)&&trim_input.Any(char.IsUpper)))
+                {
+                    return true;
+                }
+                
+            }
+
+            return true;
         }
     }
 }
